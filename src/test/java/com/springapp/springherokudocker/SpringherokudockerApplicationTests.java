d2e05db3c1a3f93d8387b89ed6d092c72321a095
glookie <na>
2021-08-29 21:53:02 +0530

    Testing configuration

--- src/test/java/com/springapp/springherokudocker/SpringherokudockerApplicationTests.java
+++ src/test/java/com/springapp/springherokudocker/SpringherokudockerApplicationTests.java
@@ -2,8 +2,10 @@ package com.springapp.springherokudocker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class SpringherokudockerApplicationTests {
 
 	@Test
