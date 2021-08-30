407a12249238e646609ae13fb6967034672f8198
glookie <na>
2021-08-30 16:43:59 +0530

    Added environment property and fixed travis env var

--- src/main/java/com/springapp/springherokudocker/SpringHerokuController.java
+++ src/main/java/com/springapp/springherokudocker/SpringHerokuController.java
@@ -3,23 +3,29 @@ package com.springapp.springherokudocker;
 import java.time.Instant;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringHerokuController {
     
+    @Autowired
+    Environment env;
+
     @GetMapping("/date")
     public Date getdate() {
         return Date.from(Instant.now());
     }
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String getInfo(){
-        if(System.getenv("MYCUSTOM")!=null)
+
+        if(System.getenv("MYCUSTOM")!=null && System.getenv("MYCUSTOM")!="")
             return System.getenv("MYCUSTOM");
         else
-            return "Hello";
+            return env.toString();
         
     }
 }
